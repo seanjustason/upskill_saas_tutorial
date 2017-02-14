0edb45c3a68739341c2be1e5d297582df7229d54
Sean Justason <seanjustason@gmail.com>
2017-02-14 10:50:26 +0000

    Added profile and user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  belongs_to :plan 
+  belongs_to :plan
+  has_one :profile
   
   attr_accessor :stripe_card_token
   # If Pro user passes validations (email, password, etc.), then call Stripe
